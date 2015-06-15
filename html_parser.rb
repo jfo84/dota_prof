a951a0804aa05b4a81abea24ce3b7b0483632ecf
jford8820 <jford8820@gmail.com>
2015-06-15 16:33:59 -0400

    cleaned up with OOP concepts

--- html_parser.rb
+++ html_parser.rb
@@ -1,38 +1,66 @@
 require 'nokogiri'
 require 'open-uri'
 
-@file = Nokogiri::HTML(open("http://www.gosugamers.net/dota2/rankings#team"))
-
-team_names = @file.css("span[class='main no-game']")
-team_names_2 = []
-team_names.each do |team|
-  team_names_2 << team.children[-1].text
+def scrape_html
+  @file ||= Nokogiri::HTML(open("http://www.gosugamers.net/dota2/rankings#team"))
 end
 
-team_ids = @file.css("tr[class='ranking-link']")
-team_ids_2 = []
-team_ids.each do |team_data|
-  team_ids_2 << team_data.attributes["data-id"].text
-end
-
-team_hash = Hash[team_ids_2.zip(team_names_2.map {|values| values})]
-team_urls = []
-team_hash.each do |team_id, name|
-  name.gsub!(" ", "-")
-  name.gsub!(".", "-")
-  team_urls << "#{team_id}-#{name.downcase}"
-end
-
-rosters_array = []
-
-team_urls.each do |team_url|
-  @file_2 = Nokogiri::HTML(open("http://www.gosugamers.net/dota2/teams/#{team_url}"))
-  roster = @file_2.css("h5")
-  roster_array = []
-  roster.each do |player|
-    roster_array << player.text
+def team_names
+  team_names = @file.css("span[class='main no-game']")
+  team_names_2 = []
+  team_names.each do |team|
+    team_names_2 << team.children[-1].text
   end
-  rosters_array << roster_array
+  team_names_2
 end
 
-@roster_hash = Hash[team_names_2.zip(rosters_array.map {|values| values})]
+def team_ids
+  team_ids = @file.css("tr[class='ranking-link']")
+  team_ids_2 = []
+  team_ids.each do |team_data|
+    team_ids_2 << team_data.attributes["data-id"].text
+  end
+  team_ids_2
+end
+
+def make_slugs
+  team_hash = Hash[team_ids.zip(team_names.map {|values| values})]
+  team_url_slugs = []
+  team_hash.each do |team_id, name|
+    name.gsub!(" ", "-")
+    name.gsub!(".", "-")
+    team_url_slugs << "#{team_id}-#{name.downcase}"
+  end
+  team_url_slugs
+end
+
+def scrape_rosters
+  rosters_array = []
+  make_slugs.each do |team_url_slug|
+    @file_2 = Nokogiri::HTML(open("http://www.gosugamers.net/dota2/teams/#{team_url_slug}"))
+    roster = @file_2.css("h5")
+    roster_array = []
+    roster.each do |player|
+      roster_array << player.text
+    end
+    rosters_array << roster_array
+  end
+  rosters_array
+end
+
+def roster_hash
+  Hash[team_names.zip(scrape_rosters.map {|values| values})]
+end
+
+def print_rosters
+  roster_hash.each_with_index do |(team_name, roster), index|
+    puts "#{index + 1}. #{team_name}"
+    roster.each do |player|
+      puts "- #{player}"
+    end
+  end
+end
+
+scrape_html
+scrape_rosters
+print_rosters
