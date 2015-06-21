41204a94bf34067c11eb517753742a5cbf641906
jford8820 <jford8820@gmail.com>
2015-06-21 18:23:52 -0400

    code works but needs cleanup and database

--- steam_api_engine.rb
+++ steam_api_engine.rb
@@ -20,20 +20,29 @@ require_relative 'assets/team_data'
 
 # redis = Redis.new
 
-TEAM_NAMES_AND_IDS[0].each do |team_name, team_id|
-  steam_data = HTTParty.get("http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=2821A34B97E539012E2EA60D19D0A917&team_id=#{team_id}")
-  sleep 1
-  matches = steam_data["result"]["matches"]
-  match_id_array = []
-  until matches[99].nil?
-    matches.each do |match_data|
-      match_id_array << match_data["match_id"] unless match_id_array.include?(match_data["match_id"])
+def get_match_ids
+  TEAM_NAMES_AND_IDS[0].each do |team_name, team_id|
+    @url = "http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=2821A34B97E539012E2EA60D19D0A917&team_id=#{team_id}"
+    steam_data = HTTParty.get(@url)
+    matches = steam_data["result"]["matches"]
+    match_id_array = []
+    until matches.length < 100
+      matches = steam_data["result"]["matches"]
+      matches.each do |match_data|
+        match_id_array << match_data["match_id"] unless match_id_array.include?(match_data["match_id"])
+      end
+      last_id = steam_data["result"]["matches"].last["match_id"]
+      @url = "http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=2821A34B97E539012E2EA60D19D0A917&team_id=#{team_id}&start_at_match_id=#{last_id}"
+      steam_data = HTTParty.get(@url)
+      sleep 2
     end
-    last_match_id = matches.last["match_id"]
-    steam_data = HTTParty.get("http://api.steampowered.com/IDOTA2Match_570/GetMatchHistory/v001/?key=2821A34B97E539012E2EA60D19D0A917&team_id=#{team_id}&start_at_match_id=#{last_match_id}")
-    sleep 1
+    top_fifty_match_ids = []
+    top_fifty_match_ids << match_id_array
   end
+  top_fifty_match_ids
 end
+
+get_match_ids
 
 # max_date=#{steam_data["result"]["matches"][99]["match_seq_num"]}
 # redis.set team_id, steam_data
