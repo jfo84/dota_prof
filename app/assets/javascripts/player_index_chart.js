3ff0104715003fe0ba6c4c04b67e177cc984b7e8
jford8820 <jford8820@gmail.com>
2015-08-02 17:46:30 -0400

    passing in values d3

---
+++ app/assets/javascripts/player_index_chart.js
@@ -0,0 +1,9 @@
+$(document).ready(function() {
+  var data = [4, 8, 15, 16, 23, 42];
+  d3.select(".chart")
+  .selectAll("div")
+    .data(data)
+  .enter().append("div")
+    .style("width", function(d) { return d * 10 + "px"; })
+    .text(function(d) { return d; });
+});
